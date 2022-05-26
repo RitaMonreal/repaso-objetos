893484439ab0b642e4f965e16c5185bb3c4c4d86
RitaMonreal <A326347@alumnos.uaslp.mx>
2022-05-26 12:17:05 -0500

    se resolvió el problema de los decimales, ya corre todos los test. Tiempo de resolución 30 hora más o menos

--- src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
+++ src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
@@ -1,8 +1,12 @@
 package uaslp.objetos.figuras;
 
+import static java.lang.Math.tan;
+import static java.lang.Math.tanh;
+
 public class PoligonoRegular {
     private double lado;
     private int numeroDeLados;
+    private double apotema;
 
     public PoligonoRegular(int numeroDeLados, double lado) {
         this.numeroDeLados=numeroDeLados;
@@ -18,7 +22,15 @@ public class PoligonoRegular {
     }
 
     public double getArea() {
-        return getPerimetro()*lado/2;
+
+        double grados;
+        double rad;
+
+        grados = 360/(numeroDeLados*2);
+        rad=Math.toRadians(grados);
+        apotema = lado/(2*tan(rad));
+
+        return (getPerimetro()*apotema)/2;
     }
 
     private double getPerimetro() {
